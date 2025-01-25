62a67c8cbaeea514e7146b58e127fe0518cd45df
Temss777 <temougrelidze777@gmail.com>
2025-01-25 23:36:13 +0400

    /feature buttons and price

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,19 +1,13 @@
 import "react";
 import Product from "./Product";
+import PropTypes from "prop-types";
 
 const ProductList = ({
-                         
-                         // eslint-disable-next-line react/prop-types
                          filteredProducts,
-                         // eslint-disable-next-line react/prop-types
                          addToCart,
-                         // eslint-disable-next-line react/prop-types
                          handleSearchChange,
-                         // eslint-disable-next-line react/prop-types
                          handleSortChange,
-                         // eslint-disable-next-line react/prop-types
                          searchQuery,
-                         // eslint-disable-next-line react/prop-types
                          sortOption,
                      }) => {
     return (
@@ -27,25 +21,27 @@ const ProductList = ({
                     className="search-input"
                     placeholder="Search products..."
                 />
-                {/* Sort Dropdown */}
+            </div>
+
+            {/* Sort */}
+            <div className="controls">
                 <select
                     value={sortOption}
                     onChange={handleSortChange}
                     className="sort-dropdown"
                 >
                     <option value="">Sort By</option>
-                    <option value="price-asc">Price (Low to High)</option>
-                    <option value="price-desc">Price (High to Low)</option>
-                    <option value="name-asc">Name (A-Z)</option>
-                    <option value="name-desc">Name (Z-A)</option>
+                    <option value="price-asc">Price: Low to High</option>
+                    <option value="price-desc">Price: High to Low</option>
+                    <option value="name-asc">Name: A to Z</option>
+                    <option value="name-desc">Name: Z to A</option>
                 </select>
             </div>
 
+
             {/* Product List */}
             <div className="product-list">
-                {/* eslint-disable-next-line react/prop-types */}
                 {filteredProducts.length > 0 ? (
-                    // eslint-disable-next-line react/prop-types
                     filteredProducts.map((product, index) => (
                         <div key={index} className="product-item">
                             <Product {...product} />
@@ -65,4 +61,20 @@ const ProductList = ({
     );
 };
 
-export default ProductList;
+            // PropTypes
+            ProductList.propTypes = {
+            filteredProducts: PropTypes.arrayOf(
+            PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            price: PropTypes.number.isRequired,
+        })
+            ).isRequired,
+            addToCart: PropTypes.func.isRequired,
+            handleSearchChange: PropTypes.func.isRequired,
+            handleSortChange: PropTypes.func.isRequired,
+            searchQuery: PropTypes.string.isRequired,
+            sortOption: PropTypes.string.isRequired,
+        };
+
+            export default ProductList;
